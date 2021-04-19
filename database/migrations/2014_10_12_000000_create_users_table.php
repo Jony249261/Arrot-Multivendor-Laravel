79ca802438d0ff7943ef8a93180ea85353eafa97
Azizul <azizul656589@gmail.com>
2021-04-19 11:41:19 +0600

    user table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+             $table->unsignedBigInteger('buyer_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->unsignedBigInteger('role')->nullable();
+            $table->unsignedBigInteger('status_id')->nullable();
             $table->string('password');
+            $table->string('verification_code')->nullable();
+            $table->integer('is_verified')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
