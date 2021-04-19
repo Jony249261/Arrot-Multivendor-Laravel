2c2fd9b2c9e29cf2bbc016d7734b7edbf15e261f
Azizul <azizul656589@gmail.com>
2021-04-19 15:03:02 +0600

    image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('role')->nullable();
             $table->unsignedBigInteger('status_id')->nullable();
             $table->string('password');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('verification_code')->nullable();
             $table->integer('is_verified')->default(0);
             $table->timestamp('email_verified_at')->nullable();
