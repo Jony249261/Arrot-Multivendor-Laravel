6693de2fa51c06dcce5a382faaed30687ca68da8
Md. Jony Islam <jony.just.cse@gmail.com>
2021-04-20 21:41:48 +0600

    problem

--- resources/views/supplier/buyer/create.blade.php
+++ resources/views/supplier/buyer/create.blade.php
@@ -2,18 +2,6 @@
 
 @section('buyer','active')
 @section('create','active')
-
-@section('page-styles')
-  
-
-    <!-- Bootstrap Material Datetime Picker Css -->
-    <link href="{{ asset('backend/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css') }}" rel="stylesheet" />
-
-    <!-- Custom Css -->
-    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
-
-    
-@endsection
 @section('content')
     <div class="container-fluid">
 
@@ -169,7 +157,7 @@
                                     <div class="form-group from-float">
                                         <div class="">
                                             <label class="form-label">Enter Passport Expire Date</label>
-                                            <input type="text" class="datepicker form-control @error('passport_expire_date') is-invalid @enderror" name="passport_expire_date" value="{{old('passport_expire_date')}}" placeholder="Please choose a date...">
+                                            <input type="date" class=" form-control @error('passport_expire_date') is-invalid @enderror" name="passport_expire_date" value="{{old('passport_expire_date')}}" placeholder="Please choose a date...">
                                             @error('passport_expire_date')
                                         <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -216,7 +204,7 @@
                                 <div class="form-group form-float">
 
                                     <div class="form-line">
-                                        <input type="date" class="datepicker form-control @error('expire_date') is-invalid @enderror" value="{{old('expire_date')}}" name="expire_date"  required>
+                                        <input type="date" class=" form-control @error('expire_date') is-invalid @enderror" value="{{old('expire_date')}}" name="expire_date"  required>
 
                                         @error('expire_date')
                                         <span class="invalid-feedback" role="alert">
@@ -278,18 +266,4 @@
         </div>
 @endsection
 
-
-@section('page-scripts')
-        <!-- Jquery Core Js -->
-    <script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Autosize Plugin Js -->
-    <script src="{{ asset('backend/plugins/autosize/autosize.js') }}"></script>
-    <!-- Bootstrap Material Datetime Picker Plugin Js -->
-    <script src="{{ asset('backend/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script>
-    <!-- Custom Js -->
-    <script src="{{ asset('backend/js/admin.js') }}"></script>
-    <script src="{{ asset('backend/js/pages/forms/basic-form-elements.js') }}"></script>
-
-    
-@endsection
 
--- resources/views/supplier/seller/create.blade.php
+++ resources/views/supplier/seller/create.blade.php
@@ -1,15 +1,5 @@
 @extends('layouts.supplier-app')
-@section('page-styles')
-  
 
-    <!-- Bootstrap Material Datetime Picker Css -->
-    <link href="{{ asset('backend/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css') }}" rel="stylesheet" />
-
-    <!-- Custom Css -->
-    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
-
-    
-@endsection
 @section('seller','active')
 @section('create','active')
 
@@ -31,7 +21,7 @@
 
                             <div class="row clearfix">
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                         <div class="form-line">
                                             <input type="text"  class="form-control @error('seller_name') is-invalid @enderror" name="seller_name" required>
                                             @error('seller_name')
@@ -44,20 +34,21 @@
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                         <div class="form-line">
+                                            <label class="form-label">Enter Phone Number</label>
                                             <input type="number"  class="form-control @error('seller_telephone') is-invalid @enderror" name="seller_telephone" required>
                                             @error('seller_telephone')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                         </span>
                                             @enderror
-                                            <label class="form-label">Enter Phone Number</label>
+                                            
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                     <div class="form-line">
                                         <input type="email" class="form-control @error('seller_email') is-invalid @enderror" name="seller_email" required>
                                         @error('seller_email')
@@ -79,7 +70,7 @@
                                 </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                     <div class="form-line">
                                         <input type="text" class="form-control" name="seller_website" required>
                                         <label class="form-label">Enter Seller Website</label>
@@ -87,10 +78,10 @@
                                 </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group from-float">
+                                    <div class="form-group form-float">
                                         <div class="">
                                             <label class="form-label">Enter Passport Expire Date</label>
-                                            <input type="text" class="datepicker form-control" placeholder="Please choose a date...">
+                                            <input type="date" class=" form-control" placeholder="Please choose a date...">
                                             
                                         </div>
                                     </div>
@@ -107,7 +98,7 @@
                             </div>
                             <div class="row clearfix">
                                 <div class="col-sm-6">
-                                        <div class="form-group">
+                                        <div class="form-group form-float">
                                     <div class="form-line">
                                         <input type="text" class="form-control" name="sr_name" required>
                                         <label class="form-label">Seller Representive Name</label>
@@ -115,7 +106,7 @@
                                 </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                     <div class="form-line">
                                         <input type="email" class="form-control" name="sr_email" required>
                                         <label class="form-label">Seller Representive  Email</label>
@@ -123,34 +114,25 @@
                                 </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                     <div class="form-line">
-                                        <input type="number" class="form-control" name="sr_phone" required>
                                         <label class="form-label">Seller Phone Number</label>
+                                        <input type="number" class="form-control" name="sr_phone" required>
+                                        
                                     </div>
                                 </div>
                                 </div>
                                 <div class="col-sm-6">
-                                    <div class="form-group">
+                                    <div class="form-group form-float">
                                     <div class="">
-                                        <input type="file" class="form-control" name="sr_image" required>
                                         <label class="form-label">Seller Representive Image</label>
+                                        <input type="file" class="form-control" name="sr_image" required>
+                                        
                                     </div>
                                 </div>
                                 </div>
                                 
                             </div>
-
-
-
-
-                            
-                                
-                                
-
-                                
-                                
-                                
                                 <button class="btn btn-success waves-effect" type="submit">Create Seller</button>
                             
                             
@@ -162,17 +144,5 @@
             </div>
 
         </div>
-@endsection
-@section('page-scripts')
-        <!-- Jquery Core Js -->
-    <script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Autosize Plugin Js -->
-    <script src="{{ asset('backend/plugins/autosize/autosize.js') }}"></script>
-    <!-- Bootstrap Material Datetime Picker Plugin Js -->
-    <script src="{{ asset('backend/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script>
-    <!-- Custom Js -->
-    <script src="{{ asset('backend/js/admin.js') }}"></script>
-    <script src="{{ asset('backend/js/pages/forms/basic-form-elements.js') }}"></script>
-
-    
+    </div>
 @endsection
