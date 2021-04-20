26214ebfd64ce45b358da1ad704bbe5b97dd6b14
Azizul <azizul656589@gmail.com>
2021-04-20 16:17:21 +0600

    product crud

--- app/Http/Controllers/Supplier/ProductController.php
+++ app/Http/Controllers/Supplier/ProductController.php
@@ -21,8 +21,8 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::latest()->paginate(10);
-        $prices = ProductPrice::where('updated_at',Carbon::now())->where('product_id',3);
-        dd($prices); 
+        $prices = ProductPrice::where('updated_date',date('Y-m-d'))->where('product_id',3)->first();
+        // dd($prices->sales_rate); 
         return view('supplier.product.index',compact('products'));
     }
 
@@ -74,7 +74,6 @@ class ProductController extends Controller
         // $product_price->purchase_rate = $data['purchase_rate'];
         $product_price->sales_rate = $data['sales_rate'];
         $product->productPrices()->save($product_price);
-        // $product_price->save();
        
         Session::flash('success'.'Product created successfully!!');
         return redirect()->route('products.index');
@@ -99,9 +98,9 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        $productPrice = ProductPrice::findOrFail($id);
+        $product = Product::findOrFail($id);
         $units = Unit::all();
-        return view('supplier.product.edit',compact('productPrice','units'));
+        return view('supplier.product.edit',compact('product','units'));
     }
 
     /**
@@ -113,22 +112,21 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-         dd($request->all());
+        //  dd($request->all());
         $data = $request->validate([
             'product_name' => 'required|string',
             'description' => 'required|string',
-            'purchase_rate' => 'required|numeric',
+            'purchase_rate' => 'sometimes|nullable|numeric',
             'sales_rate' => 'required|numeric',
             'image' => 'sometimes|nullable|mimes:jpeg,jpg,png,gif|required|max:10000',
             'unit_id' => 'required|numeric',
             'product_type' => 'required|string',
         ]);
         $data['product_id'] = '0001';
-        $product = new Product();
+
+        $product = Product::findOrFail($id);
         $product->product_id = $data['product_id'];
         $product->product_name = $data['product_name'];
-        // $product->slug = $data['product_name'].'-'.'testing';
-        // $product->product_code = $data['product_code'];
         $product->product_description = $data['description'];
         $product->unit_id = $data['unit_id'];
         $product->product_type = $data['product_type'];
@@ -141,12 +139,11 @@ class ProductController extends Controller
         $product->save();
 
         $product_price =new ProductPrice();
-        $product_price->product_id = $product->id;
-        $product_price->purchase_rate = $data['purchase_rate'];
         $product_price->sales_rate = $data['sales_rate'];
-        $product_price->save();
+        $product_price->updated_date = date('Y-m-d');
+        $product->productPrices()->save($product_price);
        
-        Session::flash('success'.'Product created successfully!!');
+        Session::flash('success'.'Product updated successfully!!');
         return redirect()->route('products.index');
     }
 
@@ -156,8 +153,13 @@ class ProductController extends Controller
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        // dd($product->id);
+        $product = Product::findOrFail($id);
+        $product->productPrices()->delete();
+        $product->delete();
+        Session::flash('success'.'Product deleted successfully!!');
+        return redirect()->back();
     }
 }
