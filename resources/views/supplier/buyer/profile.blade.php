61c28364fd16bfc089e4e3921727e852fbebd220
Md. Jony Islam <jony.just.cse@gmail.com>
2021-04-22 23:45:15 +0600

    profile page

--- resources/views/supplier/buyer/profile.blade.php
+++ resources/views/supplier/buyer/profile.blade.php
@@ -8,103 +8,77 @@
 
         <div class="container-fluid">
             <!-- Basic Example -->
-            <div class="row gutters-sm">
-            <div class="col-md-12 mb-3">
-              <div class="card">
+                   <div class="row gutters-sm">
+            <div class="card">
               <div class="header bg-red">
-              <h2>Buyer Information</h2>
-              </div>
-                <div class="card-body cardbody">
-                  <div class="d-flex flex-column align-items-center text-center">
-                    <img src="{{asset('image_buyer/user/'.$user->image)}}" alt="Admin" class="rounded-circle" width="150">
-                    <div class="mt-3">
-                      <h4>{{$buyer->buyer_name}}</h4>
-                      <p class="text-secondary mb-1">{{$buyer->tagline}}</p>
-                      <p class="text-muted font-size-sm">{{$buyer->buyer_address}}</p>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row gutters-sm">
-          <div class="col-md-12 text-center">
-              <div class="card">
-              <div class="header bg-red">
-              <h2>Buyer Information</h2>
+              <h2 class="text-center">
+                Buyer Information
+              </h2>
               </div>
               <br>
-                <div class="card-body">
-                  <div class="row">
-
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Name</h6>
+        <div class="card-body cardbody">
+          <div class="col-md-4">
+            <div class="row">
+                    <div class="col-sm-5">
+                      <h5 class="mb-0"></h5>
                     </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{$buyer->buyer_name}}
+                    <div class="col-sm-6 text-secondary ">
+                      <img src="{{asset('image_buyer/user/'.$user->image)}}" alt="Admin" class="rounded-circle" width="120">
                     </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Email</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                       {{$buyer->buyer_email}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Phone</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{$buyer->buyer_telephone}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Website</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{$buyer->buyer_website}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Buyer Type</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{$buyer->buyer_type}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Address</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{$buyer->buyer_address}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0 profilen">Buyer Tagline</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{$buyer->tagline}}
-                    </div>
-                  </div>
-                </div>
               </div>
+          </div>
+            <div class="col-md-4">
+              <div class="row">
+                    <div class="col-sm-12 ">
+                      <h5 class="mb-0"><span>Name:</span> {{$buyer->buyer_name}}</h5>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Email:</span> {{$buyer->buyer_email}}</h5>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Phone:</span> {{$buyer->buyer_telephone}}</h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Address:</span> {{$buyer->buyer_address}}</h5>
+                    </div>
+                  </div>
+
+
+            </div>
+            <div class="col-md-4">
+                    <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0 "><span>Tagline:</span> {{$buyer->tagline}}</h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Website:</span> {{$buyer->buyer_website}}</h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Buyer Type:</span> {{$buyer->buyer_type}}</h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Buyer Id:</span> {{$buyer->buyer_id}}</h5>
+                    </div>
+                  </div>
             </div>
 
           </div>
+          </div>
+
 
           <div class="row gutters-sm">
 
@@ -120,7 +94,7 @@
                     <div class="col-sm-4">
                       <h6 class="mb-0 profilen"></h6>
                     </div>
-                    <div class="col-sm-6 text-secondary">
+                    <div class="col-sm-6 text-secondary trade">
                      <img src="{{asset('image_buyer/user/'.$buyer->trade_license)}}" alt="Admin" class="img-thumbnail" >
                     </div>
                   </div>
@@ -139,82 +113,77 @@
             </div>
 
             <div class="col-md-6 ">
-              <div class="card">
+              <div class="row gutters-sm">
+            <div class="card card-sr">
               <div class="header bg-red">
               <h2 class="text-center">
                 Buyer Representive
               </h2>
               </div>
-                <div class="card-body cardbody">
-                  <div class="row">
-                    <div class="col-sm-4">
-                      <h6 class="mb-0 profilen"></h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      <img src="{{asset('image_buyer/user/'.$buyer->br_image)}}" alt="Admin" class="rounded-circle" width="120">
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <h6 class="mb-0 profilen">Name</h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      {{$buyer->br_name}}
+              <br>
+          <div class="card-body cardbody">
+            <div class="col-md-4">
+            <div class="row">
+                    <div class="col-sm-12 text-secondary ">
+                      <img src="{{asset('image_buyer/user/'.$buyer->br_image)}}" alt="Admin" class="rounded-circles" width="120">
                     </div>
                   </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <h6 class="mb-0 profilen">Phone</h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      {{$buyer->br_phone}}
+            </div>
+            <div class="col-md-8">
+              <div class="row">
+                    <div class="col-sm-12 ">
+                      <h5 class="mb-0"><span>Name:</span>  {{$buyer->br_name}}</h5>
                     </div>
                   </div>
-                  <hr>
+
                   <div class="row">
-                    <div class="col-sm-6">
-                      <h6 class="mb-0 profilen">Email</h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      {{$buyer->br_email}}
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Phone:</span> {{$buyer->br_phone}}</h5>
                     </div>
                   </div>
-                  <hr>
+
                   <div class="row">
-                    <div class="col-sm-6">
-                      <h6 class="mb-0 profilen">NID</h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      {{$buyer->buyer_nid}}
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Email:</span> {{$buyer->br_email}}</h5>
                     </div>
                   </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <h6 class="mb-0 profilen">Passport</h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      {{$buyer->buyer_passport}}
+                   <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0 "><span>NID:</span> {{$buyer->buyer_nid}}</h5>
                     </div>
                   </div>
-                  <hr>
                   <div class="row">
-                    <div class="col-sm-6">
-                      <h6 class="mb-0 profilen">Passport Exp-Date</h6>
-                    </div>
-                    <div class="col-sm-6 text-secondary">
-                      {{$buyer->passport_expire_date}}
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Passport:</span> {{$buyer->buyer_passport}}</h5>
                     </div>
                   </div>
-                </div>
-              </div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <h5 class="mb-0"><span>Passport Exp-Date:</span> {{$buyer->passport_expire_date}}</h5>
+                    </div>
+                  </div>
+
+
+            </div>
+
+          </div>
+          </div>
             </div>
 
 
           </div>
+
+
+
+
+
+
             </div>
+
+
+
+
+
             <!-- #END# Colored Card - With Loading -->
 
 
