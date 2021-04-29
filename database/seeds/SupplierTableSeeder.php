bd03013909c1fe7f9dc4459fc6215307c62d4120
Mahfujur Rahman Moon <37837514+Mahfujur51@users.noreply.github.com>
2021-04-29 10:48:14 +0600

    Update SupplierTableSeeder.php

--- database/seeds/SupplierTableSeeder.php
+++ database/seeds/SupplierTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
+
 
 class SupplierTableSeeder extends Seeder
 {
@@ -12,15 +12,6 @@ class SupplierTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Md Supplier Rahman',
-            'email' => 'supplier@gmail.com',
-            'password' => bcrypt('12345678'),
-            'phone'=>'01925555115',
-            'role'=>'supplier',
-            'image'=>'defaultphoto.png',
-            'is_verified'=>1
 
-        ]);
     }
 }
